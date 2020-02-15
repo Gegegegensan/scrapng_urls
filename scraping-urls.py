9f5cff9bd96c65a1c2866e4533c846dd68dc5819
Ryo <Gegegegensan@users.noreply.github.com>
2020-02-15 19:46:52 +0800

    Updating scraping_urls.py

--- scraping-urls.py
+++ scraping-urls.py
@@ -9,13 +9,15 @@ url_output = []
 
 def list_urls():
     while x < 4: # Change this number accordingly
-        page = "/pg" + str(x)
+        page = "<>" + str(x) # Add a directory name that represents next pages
         url = "URL" + page # Paste the URL you want to scrape
         headers = {'User-Agent':'Mozilla/5.0'}
         r = requests.get(url, headers=headers)
         soup = BeautifulSoup(r.text, "lxml")
         for atag in soup.find_all("a", class_="SPECIFY CLASS HERE"): # Paste the class names here
             link = atag.get("href")
+            if "<SPECIFIC STRING>" in link:
+                link.replace("<SPECIFIC STRING>", "")
             name = atag.get_text()
             output = "URL" + link, name # Paste the URL you want to scrape since link only shows directory names
             #print(output)
